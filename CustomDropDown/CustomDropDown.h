c099ea54f982bbbd8873389ead56c472210e9e88
tejuamirthi@gmail.com <tejuamirthi@gmail.com>
2020-10-04 12:57:56 +0530

    initial commit framework
    Signed-off-by: tejuamirthi@gmail.com <tejuamirthi@gmail.com>

---
+++ CustomDropDown/CustomDropDown.h
@@ -0,0 +1,19 @@
+//
+//  CustomDropDown.h
+//  CustomDropDown
+//
+//  Created by Amirthy Tejeshwar on 04/10/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomDropDown.
+FOUNDATION_EXPORT double CustomDropDownVersionNumber;
+
+//! Project version string for CustomDropDown.
+FOUNDATION_EXPORT const unsigned char CustomDropDownVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomDropDown/PublicHeader.h>
+
+
