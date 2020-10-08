d36060b5ab6c570b09381b42a594d630a40a3335
tejuamirthi@gmail.com <tejuamirthi@gmail.com>
2020-10-08 22:56:23 +0530

    Added a new target example

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Amirthy Tejeshwar on 08/10/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
