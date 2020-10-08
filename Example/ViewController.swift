17029b71840f32726aba3e6116f131c5765df5b7
tejuamirthi@gmail.com <tejuamirthi@gmail.com>
2020-10-08 22:57:37 +0530

    Example target updated with test example code
    Signed-off-by: tejuamirthi@gmail.com <tejuamirthi@gmail.com>

--- Example/ViewController.swift
+++ Example/ViewController.swift
@@ -10,11 +10,69 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let items = ["first new hey", "second name", "third name"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        let ccp = CustomDropDownPresenter<String>(items: items, delegate: self)
+        let newView = ccp.getDropDownView()
+        view.addSubview(newView)
+        newView.translatesAutoresizingMaskIntoConstraints = false
+        newView.widthAnchor.constraint(equalToConstant: view.bounds.width).isActive = true
+        newView.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        newView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        newView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
+        newView.backgroundColor = .red
         // Do any additional setup after loading the view.
     }
+}
 
+//extension ViewController: UITableViewDataSource, UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        items.count
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        let label = UILabel()
+//        label.attributedText = NSAttributedString(string: items[indexPath.row], attributes: [NSAttributedString.Key.foregroundColor : UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
+//        cell.contentView.addSubview(label)
+//        cell.contentView.backgroundColor = .gray
+//        label.addAnchors(top: cell.contentView.topAnchor, bottom: cell.contentView.bottomAnchor, left: cell.contentView.leftAnchor, right: cell.contentView.rightAnchor, padding: 8, widthConstraint: nil, heightConstraint: label.heightAnchor.constraint(greaterThanOrEqualToConstant: 32))
+//        return cell
+//    }
+//}
 
+extension ViewController: CustomDropDownDelegate, CustomDropDownDataSource {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 50
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let cell = UIView()
+        let label = UILabel()
+        label.attributedText = NSAttributedString(string: "Headersection", attributes: [NSAttributedString.Key.foregroundColor : UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
+        cell.backgroundColor = .lightGray
+        cell.addSubview(label)
+        label.addAnchors(top: cell.topAnchor, bottom: cell.bottomAnchor, left: cell.leftAnchor, right: cell.rightAnchor, padding: 8, widthConstraint: nil, heightConstraint: label.heightAnchor.constraint(greaterThanOrEqualToConstant: 32))
+        return cell
+    }
+    
+    func overrideDropDownView() -> UIView? {
+        let customView = UIView()
+        let title = UILabel()
+        let button = UIImageView()
+        customView.addSubview(title)
+        customView.addSubview(button)
+        button.image = UIImage(named: "arrowtriangle.down.fill")?.withTintColor(.gray)
+        title.addAnchors(top: customView.topAnchor, bottom: customView.bottomAnchor, left: customView.leftAnchor, right: button.leftAnchor, padding: 8, widthConstraint: nil, heightConstraint: title.heightAnchor.constraint(greaterThanOrEqualToConstant: 32))
+        title.attributedText = NSAttributedString(string: "My Custom hello", attributes: [NSAttributedString.Key.foregroundColor : UIColor.black, NSAttributedString.Key.font: UIFont.systemFont(ofSize: 16)])
+        button.addAnchors(top: customView.topAnchor, bottom: customView.bottomAnchor, left: nil, right: customView.rightAnchor, padding: 8, widthConstraint: button.widthAnchor.constraint(equalToConstant: 60), heightConstraint: nil)
+        return customView
+    }
 }
 
