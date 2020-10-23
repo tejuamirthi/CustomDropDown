89f232975abe8090b3103739b38fb39996ce79bc
Jacqueline Alves <jacque.alves.b@gmail.com>
2020-10-22 21:02:35 -0300

    Create separate files for models

---
+++ CustomDropDown/Model/DropDownMode.swift
@@ -0,0 +1,14 @@
+//
+//  DropDownMode.swift
+//  CustomDropDown
+//
+//  Created by Jacqueline Alves on 22/10/20.
+//  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
+//
+
+import Foundation
+
+public enum DropDownMode: Int, CaseIterable {
+    case label
+    case imageLabel
+}
--- CustomDropDown/Presenter/CustomDropDownPresenter.swift
+++ CustomDropDown/Presenter/CustomDropDownPresenter.swift
@@ -8,21 +8,6 @@
 
 import UIKit
 
-public struct DropDownConfig {
-    public var dropDownWidth: CGFloat?
-    public var dropDownTag: Int = 999
-    public var selectedLabelTag: Int = 9999
-    public var dropDownLeftRightPadding: UIEdgeInsets = UIEdgeInsets(top: 10, left: 10, bottom: 0, right: 10)
-    public var dropDownMode: DropDownMode = .label
-    
-    public init() {}
-}
-
-public enum DropDownMode: Int, CaseIterable {
-    case label
-    case imageLabel
-}
-
 public protocol CustomDropDownDataSource: class {
     func overrideDropDownView(identifier: Int) -> UIView?
     func config(identifier: Int) -> DropDownConfig
