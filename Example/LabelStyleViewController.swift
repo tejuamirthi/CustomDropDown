e29502ca7f3944242c89a94fe67e049beee5c459
devanshim <devanshim@hyperlinkinfosystem.net.in>
2020-10-28 10:25:09 +0530

    Resolve Conflicts

--- Example/LabelStyleViewController.swift
+++ Example/LabelStyleViewController.swift
@@ -5,62 +5,24 @@
 //  Created by Amirthy Tejeshwar on 08/10/20.
 //  Copyright © 2020 Amirthy Tejeshwar. All rights reserved.
 //
-
 import UIKit
 import CustomDropDown
 
 class LabelStyleViewController: UIViewController {
 
-    let items = ["first item", "second name", "third name", "fourth item", "fifth name", "sixth name", "seventh item", "eight name", "ninth name"]
-    
-    let imageItems = [ImageLabelData(title: "Football", image: UIImage(named: "football")), ImageLabelData(title: "Rugby", image: UIImage(named: "rugby"))]
+    let items = ["Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", "second name", "third name", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", "second name", "third name", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", "second name", "third name", "Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.", "second name", "third name"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-<<<<<<< HEAD:Example/ViewController.swift
-        view.backgroundColor = .systemTeal
-        
-        
-        
-//        let ccpImage = CustomDropDownPresenter<ImageLabelData>(items: imageItems, delegate: self)
-//        let newViewImage = ccpImage.getDropDownView()
-//        view.addSubview(newViewImage)
-
-//        let centerVerticallyImage = NSLayoutConstraint(item: newViewImage, attribute: .centerX, relatedBy: .equal,
-//                                                  toItem: view, attribute: .centerX, multiplier: 1, constant: 0.0)
-//        let centerHorizontallyImage = NSLayoutConstraint(item: newViewImage, attribute: .centerY, relatedBy: .equal,
-//                                                    toItem: view,  attribute: .centerY, multiplier: 1.3, constant: 0)
-//        NSLayoutConstraint.activate([centerVerticallyImage, centerHorizontallyImage])
-//        newViewImage.translatesAutoresizingMaskIntoConstraints = false
-//        newViewImage.widthAnchor.constraint(equalToConstant: view.bounds.width).isActive = true
-//        newViewImage.heightAnchor.constraint(equalToConstant: 100).isActive = true
-//        newViewImage.backgroundColor = .orange
-        
-        
-=======
         view.backgroundColor = .white
->>>>>>> main:Example/LabelStyleViewController.swift
         let ccp = CustomDropDownPresenter<String>(items: items, delegate: self)
         let newView = ccp.getDropDownView()
         view.addSubview(newView)
-//
-//        let centerVertically = NSLayoutConstraint(item: newView, attribute: .centerX, relatedBy: .equal,
-//                                                  toItem: view, attribute: .centerX, multiplier: 1, constant: 0.0)
-//        let centerHorizontally = NSLayoutConstraint(item: newView, attribute: .centerY, relatedBy: .equal,
-//                                                    toItem: view, attribute: .centerY, multiplier: 0.5, constant: 0)
-//        NSLayoutConstraint.activate([centerVertically, centerHorizontally])
         newView.translatesAutoresizingMaskIntoConstraints = false
         newView.widthAnchor.constraint(equalToConstant: view.bounds.width).isActive = true
         newView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         newView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         newView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-<<<<<<< HEAD:Example/ViewController.swift
-        newView.backgroundColor = .red
-    }
-}
-
-extension ViewController: CustomDropDownDelegate, CustomDropDownDataSource {
-=======
         newView.backgroundColor = .darkGray
         // Do any additional setup after loading the view.
     }
@@ -81,16 +43,15 @@ extension ViewController: CustomDropDownDelegate, CustomDropDownDataSource {
 //        return cell
 //    }
 //}
-
 extension LabelStyleViewController: CustomDropDownDelegate, CustomDropDownDataSource {
     
->>>>>>> main:Example/LabelStyleViewController.swift
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, data: Any, identifier: Int) {
         
     }
     
     func config(identifier: Int) -> DropDownConfig {
         var config = DropDownConfig()
+        
         var shadowAndCornerConfig = ShadowAndCornerConfig()
         shadowAndCornerConfig.shadowOffset = CGSize(width: 5, height: 5)
         shadowAndCornerConfig.shadowOpacity = 0.8
