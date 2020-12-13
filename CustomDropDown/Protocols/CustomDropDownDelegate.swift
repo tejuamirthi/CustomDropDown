01d15600969988e4388dda3068e1288e4c64eac0
tejuamirthi <tejuamirthi@gmail.com>
2020-12-13 12:03:18 +0530

    Custom Drop down delegate

--- CustomDropDown/Protocols/CustomDropDownDelegate.swift
+++ CustomDropDown/Protocols/CustomDropDownDelegate.swift
@@ -10,27 +10,20 @@ import UIKit
 
 public protocol CustomDropDownDelegate: class {
     // Use in case of custom Implementation
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, displayView: UIView, config: DropDownConfig, data: Any, identifier: Int)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, displayView: UIView, data: Any?, identifier: Int)
     
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, data: Any, identifier: Int)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, data: Any?, identifier: Int)
 }
 
 public extension CustomDropDownDelegate where Self: NSObject {
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, displayView: UIView, config: DropDownConfig, data: Any, identifier: Int) {
-        self.tableView(tableView, didSelectRowAt: indexPath, data: data, identifier: identifier)
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath, displayView: UIView, data: Any?, identifier: Int) {
         
-        var selectedString: String? = nil
         
-        switch config.dropDownMode {
-        case .imageLabel:
-            selectedString = (data as? ImageLabelData)?.title
-        default:
-            selectedString = data as? String
-        }
         
-        if let text = selectedString,
-           let label = displayView.viewWithTag(config.selectedLabelTag) as? UILabel {
-            label.text = text
-        }
+        
+        
+        
+        
+        
     }
 }
